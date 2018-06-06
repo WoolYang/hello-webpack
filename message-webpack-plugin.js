039e56c597750e80030e475eedb5f9c4b8b20a68
WoolYang <zhaoy_1992@163.com>
2018-06-06 18:48:49 +0800

    打印信息插件

---
+++ message-webpack-plugin.js
@@ -0,0 +1,53 @@
+const colors = require("ansi-colors");
+const cClear = require("console-clear");
+const format = require("webpack-format-messages");
+
+const log = str => console.log(str); // eslint-disable-line
+const clear = () => (cClear(), true);
+
+class WebpackMessages {
+    constructor(opts) {
+        opts = opts || {};
+        this.name = opts.name || "project";
+        this.onDone = opts.onComplete;
+        this.logger = opts.logger || log;
+    }
+
+    printError(str, arr) {
+        arr && (str += "\n\n" + arr.join(""));
+        clear() && this.logger(str);
+    }
+
+    apply(compiler) {
+        const name = this.name ? ` ${colors.cyan(this.name)} bundle` : "";
+        const onStart = () => this.logger(`${colors.blueBright(`Building${name}...`)}`);
+        const onReady = () => this.logger(`${colors.blueBright(`Ready to output ${name}... `)}`);
+
+        compiler.plugin("compile", onStart);
+        compiler.plugin("invalid", () => clear() && onStart());
+        compiler.plugin("emit", (compilation, callback) => {
+            onReady();
+            callback();
+        });
+        compiler.plugin("done", stats => {
+            const messages = format(stats);
+
+            if (messages.errors.length) {
+                return this.printError(colors.red(`Failed to compile${name}!`), messages.errors);
+            }
+
+            if (messages.warnings.length) {
+                return this.printError(colors.yellow(`Compiled${name} with warnings.`), messages.warnings);
+            }
+
+            if (this.onDone !== undefined) {
+                this.onDone(name, stats);
+            } else {
+                const sec = (stats.endTime - stats.startTime) / 1e3;
+                this.logger(colors.green(`Completed${name} in ${sec}s!`));
+            }
+        });
+    }
+}
+
+module.exports = WebpackMessages;
